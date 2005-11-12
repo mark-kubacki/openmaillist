be1133a855c23556f95a2f4aaa92bbb853709cd4
Alex Wall <alex@noligy.de>
2005-11-12 18:05:38 +0000

    OML creating classes for data fetch
    git-svn-id: file:///var/svn/openmaillist/trunk@9 af08cab8-d604-0410-a591-b642d820b6c0

--- inc/lib/openmaillist.php
+++ inc/lib/openmaillist.php
@@ -1,25 +1,21 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-		"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<title>OML</title>
-	</head>
-	
-	<body>
-<b>Lists</b>
-		<?php
+<?php
 
 	
-		// Connecting, selecting database
+class openmaillist {
+
+	// Connecting, selecting database
 		
 		$link = mysql_connect($host, $user, $pass)
 		or die('Could not connect: ' . mysql_error());
 		//echo 'Connected successfully';
 		mysql_select_db('test') or die('Could not select database');
 		
-		// Performing SQL query
-		$query = '
+	function get_lists() {
+		global $cfg;
+		$tmp	= array();
 
+	
+		$result = mysql_query('
 		select li.LID, lname as Name, LEmailTo as EmailTo, LDescription as Description,
 			count(th.TID) as Threads, 
 			count(tm.TID) as Posts
@@ -27,43 +23,29 @@
 		left outer join Threads th on (li.LID = th.LID)
 		left outer join ThreadMessages tm on (th.TID = tm.TID)
 		group by li.lname, li.LEmailTo, li.LDescription
-		-- Name 	Status 	Threads 	Posts 	Last post
+		-- Name Status 	Threads Posts 	Last post
+		');
 
-';
-		$result = mysql_query($query) or die('Query failed: ' . mysql_error());
-		
-		// Printing results in HTML
-		echo "<table>\n";
-		while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
-		echo "\t<tr>\n";
-		foreach ($line as $key => $col_value) {
-		echo "\t\t<td><a href=\"http://".$_SERVER["HTTP_HOST"].$_SERVER["PHP_SELF"]."?list=".$line["LID"]."\">".$col_value."</a></td>\n";
+		if(mysql_num_rows($result) > 0) {
+		while($row = mysql_fetch_assoc($result)) {
+			$tmp[] = $row;
 		}
-		echo "\t</tr>\n";
-		}
-		echo "</table>\n";
-		
-		// Free resultset
 		mysql_free_result($result);
-		
-		// Closing connection
-		mysql_close($link);
-		?> 
+		}
+		return $tmp;
+		}
 
-<br><br><br>
-<!-- ############################################################################################ -->
-<b>Threads</b>		<?php
+		
+
+
+
+
+	function get_threads() {
+		global $cfg;
+		$tmp	= array();
+
 	
-		if ($_GET["list"] != NULL){
-		// Connecting, selecting database
-		
-		$link = mysql_connect($host, $user, $pass)
-		or die('Could not connect: ' . mysql_error());
-		//echo 'Connected successfully';
-		mysql_select_db('test') or die('Could not select database');
-		
-		// Performing SQL query
-		$query = '
+		$result = mysql_query('
 		select th.TID,th.Threadname as Subject, tm.Sender, count(tm.tid) as Posts 
 		from Threads th
 		left outer join ThreadMessages tm on (th.tid = tm.tid) 
@@ -72,45 +54,22 @@
 		group by th.Threadname, tm.Sender
 		order by tm.DateReceived
 		-- Subject 	Status 	Posts 	Views 	Sender 	Last post
-';
-		$result = mysql_query($query) or die('Query failed: ' . mysql_error());
-		
-		// Printing results in HTML
-		echo "<table>\n";
-		while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
-		echo "\t<tr>\n";
-		foreach ($line as $col_value) {
-		echo "\t\t<td><a href=\"http://".$_SERVER["HTTP_HOST"].$_SERVER["PHP_SELF"]."?list=".$_GET["list"]."&thread=".$line["TID"]."\">".$col_value."</a></td>\n";
+		';
+		if(mysql_num_rows($result) > 0) {
+		while($row = mysql_fetch_assoc($result)) {
+			$tmp[] = $row;
 		}
-		echo "\t</tr>\n";
-		}
-		echo "</table>\n";
-		
-		// Free resultset
 		mysql_free_result($result);
-		
-		// Closing connection
-		mysql_close($link);
 		}
-		?> 
+		return $tmp;
+		}
 
+ //href=\"http://".$_SERVER["HTTP_HOST"].$_SERVER["PHP_SELF"]."?list=".$_GET["list"]."&thread=".$line["TID"]."\">".$col_value."</a></td>\n";
+	function get_messages() {
 
-<!-- ############################################################################################ -->
-<br><br><br>
-<!-- ############################################################################################ -->
-<b>Messages</b>
-		<?php
 		if ($_GET["list"] != NULL && $_GET["thread"] != NULL){
-		// Connecting, selecting database
 		
-		$link = mysql_connect($host, $user, $pass)
-		or die('Could not connect: ' . mysql_error());
-		//echo 'Connected successfully';
-		mysql_select_db('test') or die('Could not select database');
-		
-		// Performing SQL query
-		$query = '
-
+		$result = mysql_query('
 		select tm.TID , tm.Sender, tm.DateSend as "Send at", tm.DateReceived as "Received at",
 			me.Subject as "Subject",  me.body as "Text", 
 			att.location as "Location"
@@ -120,47 +79,21 @@
 		where '.$_GET["thread"].' = tm.TID
 		order by tm.DateReceived
 
-';
-		$result = mysql_query($query) or die('Query failed: ' . mysql_error());
-		
-		// Printing results in HTML
-		echo "<table>\n";
-		while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
-		echo "\t<tr>\n";
-		foreach ($line as $col_value) {
-		echo "\t\t<td>".$col_value."</td>\n";
+		') ;
 
+		if(mysql_num_rows($result) > 0) {
+		while($row = mysql_fetch_assoc($result)) {
+			$tmp[] = $row;
 		}
-		echo "\t</tr>\n";
-		}
-		echo "</table>\n";
-		
-		// Free resultset
 		mysql_free_result($result);
-		
-		// Closing connection
-		mysql_close($link);
 		}
-		?> 
-<!-- ############################################################################################ -->
-<!-- ############################################################################################ -->
+		return $tmp;
+		}
 
+		//echo "\t\t<td>".$col_value."</td>\n";
 
+	}
 
+}
 
-
-
-
-
-<!--
-	<ul style="list-style-type:none" >
-		<li>Eins</li>
-		<li>Zwei</li>
-		<li>Drei</li>
-	</ul>
--->
-
-
-
-</body>
-</html>
+?>
