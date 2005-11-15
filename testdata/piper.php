91d6dfd0ebdfa8de8d4f1f1dd38174d30c9894b8
W-Mark Kubacki <wmark@hurrikane.de>
2005-11-15 20:18:39 +0000

    Another version of piper.*

--- testdata/piper.php
+++ testdata/piper.php
@@ -1,45 +1,17 @@
 #!/bin/env php
 <?php
-define(MODE_HEADER,	1);
-define(MODE_BODY,	2);
-define(MODE_ATT,	4);
+include('Mail/mimeDecode.php');
 
-$i		= 0;
-$mode	= MODE_HEADER;
+$input = fread(STDIN, 32000);
 
-$header	= '';
-$body	= '';
-$attachement	= array();
-$fields	= array();
+$decode = new Mail_mimeDecode($input, "\r\n");
+$structure = $decode->decode();
 
-while(!feof(STDIN)) {
-	$line = fgets(STDIN, 1024);
-	switch($mode) {
-		case MODE_HEADER:
-				if(trim($line) == '') {
-					$mode	= MODE_BODY;
-					preg_match_all('/([\w\-]+):\s([^\n]+(?:\n\s+[^\n]+)*)/', $header, $arr);
-				}
-				else {
-					$header	.= $line;
-				}
-			break;
-		case MODE_BODY:
-			$body .= $line;
-			break;
-		case MODE_ATT:
-			break;
-	}
-}
-
-$gesucht		= array('Message-ID', 'From', 'In-Reply-To', 'References', 'Subject');
+$gesucht		= array('message-id', 'from', 'in-reply-to', 'references', 'subject');
 
 echo("--- Datenbankrelevante Felder ---\n");
 foreach($gesucht as $was) {
-	$i = array_search($was, $arr[1]);
-	echo($arr[1][$i]." : ".$arr[2][$i]."\n");
+	echo($structure->headers[$was]."\n");
 }
-echo("--- Body ---");
-echo($body);
 
 ?>
