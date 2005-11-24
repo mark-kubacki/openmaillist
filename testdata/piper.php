132b1d2ce13979bfe435b2e1acc82a58bf7d2266
W-Mark Kubacki <wmark@hurrikane.de>
2005-11-24 18:11:14 +0000

    Date conversion in piper.php.
    git-svn-id: file:///var/svn/openmaillist/trunk@23 af08cab8-d604-0410-a591-b642d820b6c0

--- testdata/piper.php
+++ testdata/piper.php
@@ -13,5 +13,15 @@ echo("--- Datenbankrelevante Felder ---\n");
 foreach($gesucht as $was) {
 	echo($structure->headers[$was]."\n");
 }
+// Daten herausfinden
+foreach($structure->headers['received'] as $rec_raw) {
+	$zeit_raw = substr(strrchr($rec_raw, ';'), 2);
+	$zeit_nix = strtotime($zeit_raw);
+	$zeit_rev = date('r', $zeit_nix);
+	echo("$zeit_raw\t\t->\t$zeit_nix\t->\t$zeit_rev\n");
+}
+
+// echo("\n\n--- gesamt ---\n");
+// print_r($structure->headers);
 
 ?>
