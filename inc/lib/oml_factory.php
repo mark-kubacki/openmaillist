dac1ae23033663fa1fb7df8958ede89688cbb770
W-Mark Kubacki <wmark@hurrikane.de>
2005-12-08 18:16:58 +0000

    Class oml_list was added and openmaillist modified according to it.
    git-svn-id: file:///var/svn/openmaillist/trunk@63 af08cab8-d604-0410-a591-b642d820b6c0

--- inc/lib/oml_factory.php
+++ inc/lib/oml_factory.php
@@ -12,18 +12,33 @@ class oml_factory
 		$this->tables	= $table_names;
 	}
 
-	public function get_message($mid = null) {
-		$tmp = new oml_message($this->db, $this, $this->tables['Messages']);
-		if(!is_null($mid)) {
-			$tmp->assign_mid($mid);
+	public function get_all_lists() {
+		return oml_list::get_all_lists($this->db, $this, $this->tables['Lists']);
+	}
+
+
+	public function get_list($lid = null) {
+		$tmp = new oml_list($this->db, $this, $this->tables['Lists']);
+		if(!is_null($lid)) {
+			$tmp->set_unique_value($lid);
 		}
 		return $tmp;
 	}
 
+/*
 	public function get_thread($tid = null) {
 		$tmp = new oml_thread($this->db, $this, $this->tables['Threads']);
 		if(!is_null($tid)) {
-			$tmp->assign_tid($mid);
+			$tmp->set_unique_value($tid);
+		}
+		return $tmp;
+	}
+*/
+
+	public function get_message($mid = null) {
+		$tmp = new oml_message($this->db, $this, $this->tables['Messages']);
+		if(!is_null($mid)) {
+			$tmp->set_unique_value($mid);
 		}
 		return $tmp;
 	}
