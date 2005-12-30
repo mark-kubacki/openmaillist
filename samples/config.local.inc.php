a05b217c78cca0fb2643b71e5dc0c37797cd9f32
W-Mark Kubacki <wmark@hurrikane.de>
2005-12-30 18:24:02 +0000

    Example config had carried obsolete settings. That was changed.

--- samples/config.local.inc.php
+++ samples/config.local.inc.php
@@ -1,9 +1,13 @@
 <?php
+// copy this file into /inc/ and modify it
+
+$cfg['AbsoluteUri']	= 'http://127.0.0.1/openmaillist/';
+
 $i = 0;
 $cfg['Servers']['verbose'][] = 'localhost';
 $cfg['Servers']['number'][] = $i++;
 $cfg['Servers']['DB'][] = array(
-	'TYPE'	=> 'mysql',			// currently only mysql
+	'TYPE'	=> 'mysql',
 	'HOST'	=> 'localhost',
 	'USER'	=> '##MysqlUser##',
 	'PASS'	=> '##MysqlSecret##',
