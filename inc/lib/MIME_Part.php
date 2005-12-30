e6595b9f8808f09e3fdbfe4eefabe1272e7fa067
W-Mark Kubacki <wmark@hurrikane.de>
2005-12-30 13:19:38 +0000

    Email class has been rewritten. This closes #21. __No present MIME handling libraries__ I have found, including PEAR::Mail_Mime and many others, handle MIME emails correctly or do realize the recursive structure. These two classes will be definitely worth being used in other projects...
    git-svn-id: file:///var/svn/openmaillist/trunk@145 af08cab8-d604-0410-a591-b642d820b6c0

---
+++ inc/lib/MIME_Part.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Interestingly, MIME parts can be nested. Any email is a MIME part.
+ *
+ * @author		W-Mark Kubacki; wmark@hurrikane.de
+ * @version		$LastChangedDate$ $LastChangedBy$
+ * @see			<a href="http://www.ietf.org/rfc/rfc1341.txt">RFC 1341</a>
+ */
+class MIME_Part
+{
+	protected	$header;
+	protected	$body;
+
+	/** Regexp, divides header and body parts. */
+	const	rex_header_body_split	= '/(.*?)\r?\n\r?\n(.*)/s';
+	/** Regexp, detects "key: value" pairs without multiline values. */
+	const	rex_key_value_nm	= '/([^:\s]+):\s+(.*)/';
+	/** Regexp, detects "key: value" pairs with multiline values. */
+	const	rex_key_value_ml	= '/([^:\s]+):\s([^\r\n]+(?:\r?\n\s+[^\r\n]+)*)/';
+	/** This one catches boundaries. */
+	const	rex_boundary		= '/boundary=\"(.*)\"/s';
+
+	/**
+	 * @throw		InvalidArgumentException if raw_message cannot be split into header and body.
+	 */
+	public function __construct($raw_part, $recursive = true) {
+		if(preg_match(MIME_Part::rex_header_body_split, $raw_part, $arr)) {
+			unset($arr[0]);
+			$this->header	= $this->make_header_hash($arr[1]);
+			$this->body	= trim($arr[2]);
+			unset($arr);
+			$this->decode_body();
+			if($recursive) {
+				$this->recurse_if_multipart();
+			}
+		} else {
+			throw new InvalidArgumentException();
+		}
+	}
+
+	/**
+	 * @return		Hash with headers.
+	 */
+	private function make_header_hash($raw_header_part) {
+		$tmp	= array();
+		if(preg_match_all(MIME_Part::rex_key_value_ml, $raw_header_part, $arr)) {
+			unset($arr[0]);
+			for($i = 0; isset($arr[1][$i]); $i++) {
+				$key	= strtolower($arr[1][$i]);
+				$value	= $arr[2][$i];
+				if(isset($tmp[$key])) {
+					if(!is_array($tmp[$key])) {
+						$tmp[$key] = array($tmp[$key]);
+					}
+					array_push($tmp[$key], $value);
+				} else {
+					$tmp[$key] = $value;
+				}
+			}
+		}
+		return $tmp;
+	}
+
+	private function decode_body() {
+		if(isset($this->header['content-transfer-encoding'])) {
+			switch($this->header['content-transfer-encoding']) {
+				case 'quoted-printable':
+				case '7bit':
+					if(function_exists('imap_qprint')) {
+						$this->body	= imap_qprint($this->body);
+					} else {
+						$this->body	= quoted_printable_decode($this->body);
+					}
+					break;
+				case 'base64':
+					$this->body	= base64_decode($this->body);
+					break;
+				case '8bit':
+				default:
+					// do nothing
+					break;
+			}
+		}
+	}
+
+	/**
+	 * @return		Boolean whether further recursion was successfull. Always true if none was needed.
+	 */
+	private function recurse_if_multipart() {
+		if(isset($this->header['content-type']) && strstr($this->header['content-type'], 'multipart')) {
+			if(!preg_match(MIME_Part::rex_boundary, $this->header['content-type'], $arr)) {
+				return false;
+			}
+			$boundary	= str_replace(array("\n", "\r"), array('', ''), $arr[1]);
+			$arr_body	= explode('--'.$boundary, $this->body);
+			// [preamble] + {parts} + suffix -- else someone's mailer is messed up
+			if(count($arr_body) > 2) {
+				unset($arr_body[0]);
+				unset($arr_body[count($arr_body)]);
+				array_walk($arr_body,
+					create_function('&$item,$index',
+							'$item = new MIME_Part($item);'
+							));
+				$this->body	= $arr_body;
+				return true;
+			}
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @return		Desired field's contents or false, if the field does not exist.
+	 */
+	public function __get($key) {
+		if('body' == $key) {
+			return $this->body;
+		} else {
+			if(array_key_exists($key, $this->header)) {
+				return $this->header[$key];
+			} else {
+				return false;
+			}
+		}
+	}
+
+	/**
+	 * @return		Boolean
+	 */
+	public function __isset($key) {
+		return array_key_exists($key, $this->header);
+	}
+
+}
+?>
